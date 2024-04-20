3e9ee61f5fa3b2a6350750eee446689ea4e5be72
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-04-20 14:10:02 +0300

    feat: create go forward action

---
+++ src/GoForwardAction.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AymanAlhattami\FilamentContextMenu;
+
+use Filament\Actions\Action;
+
+class GoForwardAction extends Action
+{
+    public static function getDefaultName(): ?string
+    {
+        return 'go forward';
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->label('Go forward')
+            ->translateLabel()
+            ->color('gray')
+            ->icon('heroicon-o-arrow-right')
+            ->link()
+            ->extraAttributes([
+                'x-data' => '',
+                'x-on:click' => 'window.history.forward()',
+            ]);
+    }
+}
