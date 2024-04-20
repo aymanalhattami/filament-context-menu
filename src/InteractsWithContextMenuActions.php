c5a200504b13aab9a5aa481a0aa43d74ca367c6b
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-04-20 13:18:18 +0300

    feat: cached actions

---
+++ src/InteractsWithContextMenuActions.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AymanAlhattami\FilamentContextMenu;
+
+use Closure;
+use Filament\Actions\Action;
+use Filament\Actions\ActionGroup;
+use InvalidArgumentException;
+
+trait InteractsWithContextMenuActions
+{
+    protected array $cachedContextMenuActions = [];
+
+    public function bootedInteractsWithContextMenuActions(): void
+    {
+        $this->cacheContextMenuActions();
+    }
+
+    protected function cacheContextMenuActions(): void
+    {
+        foreach ($this->getContextMenuActions() as $action) {
+
+            if (!$action instanceof Action and !$action instanceof ContextMenuDivider) {
+                throw new InvalidArgumentException('context menu action must be an instance of ' . Action::class . '.');
+            }
+
+            if($action instanceof Action) {
+                $this->cacheAction($action);
+                $this->cachedContextMenuActions[] = $action;
+            }
+        }
+    }
+
+    public function getCachedContextMenuActions(): array
+    {
+        return $this->cachedContextMenuActions;
+    }
+
+    public function getContextMenu() : ContextMenu
+    {
+        return ContextMenu::make()->actions($this->getCachedContextMenuActions());
+    }
+
+    public function getContextMenuActions(): array
+    {
+        return [];
+    }
+}
