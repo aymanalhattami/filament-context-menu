57dd44f80da3d88897e863a1ea60ca979ef23904
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-04-20 13:18:05 +0300

    style: opacity

--- resources/views/components/context-menu.blade.php
+++ resources/views/components/context-menu.blade.php
@@ -1,14 +1,16 @@
 @if(method_exists(static::class, 'getContextMenu'))
-    <div id="contextMenu" class="flex z-50 min-w-48 max-w-2xl text-neutral-800 rounded-md ring-1 ring-gray-950/5 transition bg-white text-sm fixed p-2 shadow-md dark:text-gray-200 dark:bg-gray-900 dark:ring-white/10" style="display: none;">
+    <div id="contextMenu"
+         class="flex z-50 min-w-48 max-w-2xl text-neutral-800 rounded-md ring-1 ring-gray-950/5 transition bg-white text-sm fixed p-2 shadow-md dark:text-gray-200 dark:bg-gray-900 dark:ring-white/10"
+         style="display: none;">
         @foreach(static::getContextMenu()->getActions() as $action)
             @if($action instanceof \Filament\Actions\Action)
-                <span @class([
+                <div @class([
                     'context-menu-filament-action flex gap-x-4 select-none group justify-between rounded px-2 py-1.5 hover:bg-neutral-100 outline-none pl-8 data-[disabled]:opacity-50 data-[disabled]:pointer-events-none dark:hover:bg-white/5',
                     'mt-1' => !$loop->first
                 ])>
                     {{ $action }}
-                </span>
-            @elseif($action instanceof \AymanAlhattami\FilamentContextMenu\ContentMenuDivider)
+                </div>
+            @elseif($action instanceof \AymanAlhattami\FilamentContextMenu\ContextMenuDivider)
                 <div @class([
                     "flex h-px my-1 -mx-1 bg-gray-100 dark:bg-white/5",
                     'mt-1' => !$loop->first,
@@ -18,26 +20,28 @@
     </div>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function () {
             var contextMenu = document.getElementById('contextMenu');
             var contextMenuTrigger = document.getElementsByClassName('fi-main')[0];
 
-            contextMenuTrigger.addEventListener('contextmenu', function(event) {
+            contextMenuTrigger.addEventListener('contextmenu', function (event) {
                 event.preventDefault();
                 contextMenu.style.display = 'block'; // Show the context menu
+                contextMenu.style.opacity = '0';
 
-                setTimeout(function() {
+                setTimeout(function () {
                     calculateContextMenuPosition(event);
+                    contextMenu.style.opacity = '1';
                 }, 0); // Similar to $nextTick
             });
 
-            document.addEventListener('click', function(event) {
+            document.addEventListener('click', function (event) {
                 if (!contextMenu.contains(event.target)) {
                     contextMenu.style.display = 'none'; // Hide the context menu
                 }
             });
 
-            window.addEventListener('resize', function(event) {
+            window.addEventListener('resize', function (event) {
                 contextMenu.style.display = 'none';
             });
 
