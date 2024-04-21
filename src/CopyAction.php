432601bb756af945f7b6fbc559e32906a1b43172
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-04-21 23:12:50 +0300

    chore: pint

--- src/CopyAction.php
+++ src/CopyAction.php
@@ -20,21 +20,20 @@ class CopyAction extends Action
             ->translateLabel()
             ->color('gray')
             ->icon('heroicon-o-clipboard-document')
-            ->link()
-//            ->extraAttributes([
-//                'x-data' => '',
-//                'x-on:click' => new HtmlString('
-//                    const filamentContextMenuTextToCopy = window.getSelection().toString();
-//                    navigator.clipboard.writeText(filamentContextMenuTextToCopy).then(
-//                        () => {
-//                            console.log("Text copied to clipboard");
-//                        },
-//                        (err) => {
-//                            console.error("Failed to copy text: ", err);
-//                        }
-//                    );
-//                '),
-//            ])
-        ;
+            ->link();
+        //            ->extraAttributes([
+        //                'x-data' => '',
+        //                'x-on:click' => new HtmlString('
+        //                    const filamentContextMenuTextToCopy = window.getSelection().toString();
+        //                    navigator.clipboard.writeText(filamentContextMenuTextToCopy).then(
+        //                        () => {
+        //                            console.log("Text copied to clipboard");
+        //                        },
+        //                        (err) => {
+        //                            console.error("Failed to copy text: ", err);
+        //                        }
+        //                    );
+        //                '),
+        //            ])
     }
 }
