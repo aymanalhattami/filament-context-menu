089c427fde5a57a2dcb4a067ae3575b23b4def18
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-04-25 20:24:46 +0300

    fix: check if column has context menu actions

--- resources/views/filament/tables/columns/context-menu-column.blade.php
+++ resources/views/filament/tables/columns/context-menu-column.blade.php
@@ -1,84 +1,91 @@
-<div
-    {{--  wire:ignore is to fix the problem of the dropdown menu not showing when modal does not shown --}}
-    wire:ignore
-    x-data="contextMenuComponent()"
-     x-init="init()"
-     @contextmenu="contextMenuToggle($event)"
-     @close-other-menus.window="handleCloseOtherMenus($event)"
-     class="relative w-full">
+@if(count($getContextMenuActions()))
+    <div
+        {{--  wire:ignore is to fix the problem of the dropdown menu not showing when modal does not shown --}}
+        wire:ignore
+        x-data="contextMenuComponent()"
+        x-init="init()"
+        @contextmenu="contextMenuToggle($event)"
+        @close-other-menus.window="handleCloseOtherMenus($event)"
+        class="relative w-full">
 
-    <span class="cursor-default">
+    <div>
         @include($getMainView())
-    </span>
+    </div>
 
-    <template x-teleport="body">
-        <div x-show="contextMenuOpen" @click.away="contextMenuOpen = false" x-ref="contextmenu" class="z-50 min-w-48 max-w-2xl text-neutral-800 rounded-md ring-1 ring-gray-950/5 transition bg-white text-sm fixed p-2 shadow-md dark:text-gray-200 dark:bg-gray-900 dark:ring-white/10" x-cloak>
-            @foreach($getContextMenuActions() as $action)
-                @if($action->isVisible())
-                    @if($action instanceof \AymanAlhattami\FilamentContextMenu\ContextMenuDivider)
-                        <x-filament-context-menu::divider />
-                    @endif
+        <template x-teleport="body">
+            <div x-show="contextMenuOpen" @click.away="contextMenuOpen = false" x-ref="contextmenu" class="z-50 min-w-48 max-w-2xl text-neutral-800 rounded-md ring-1 ring-gray-950/5 transition bg-white text-sm fixed p-2 shadow-md dark:text-gray-200 dark:bg-gray-900 dark:ring-white/10" x-cloak>
+                @foreach($getContextMenuActions() as $action)
+                    @if($action->isVisible())
+                        @if($action instanceof \AymanAlhattami\FilamentContextMenu\ContextMenuDivider)
+                            <x-filament-context-menu::divider />
+                        @endif
 
-                    @if(($action instanceof \Filament\Actions\Action or $action instanceof Filament\Tables\Actions\Action) and !$action instanceof \AymanAlhattami\FilamentContextMenu\ContextMenuDivider)
-                        @if($action->isVisible())
-                            <div @class([
+                        @if(($action instanceof \Filament\Actions\Action or $action instanceof Filament\Tables\Actions\Action) and !$action instanceof \AymanAlhattami\FilamentContextMenu\ContextMenuDivider)
+                            @if($action->isVisible())
+                                <div @class([
                             'context-menu-filament-action flex gap-x-4 select-none group justify-between rounded px-2 py-1.5 hover:bg-neutral-100 outline-none pl-8 data-[disabled]:opacity-50 data-[disabled]:pointer-events-none dark:hover:bg-white/5',
                             'mt-1' => !$loop->first
                         ])>
-                                {{ $action }}
-                            </div>
+                                    {{ $action }}
+                                </div>
+                            @endif
                         @endif
                     @endif
-                @endif
-            @endforeach
-        </div>
-    </template>
-</div>
+                @endforeach
+            </div>
+        </template>
+    </div>
 
-<script>
-    function contextMenuComponent() {
-        return {
-            contextMenuOpen: false,
+    <script>
+        function contextMenuComponent() {
+            return {
+                contextMenuOpen: false,
 
-            contextMenuToggle: function(event) {
-                event.preventDefault();
-                this.contextMenuOpen = true;
-                this.$refs.contextmenu.style.opacity = 0;
+                contextMenuToggle: function(event) {
+                    event.preventDefault();
+                    this.contextMenuOpen = true;
+                    this.$refs.contextmenu.style.opacity = 0;
 
-                this.$dispatch('close-other-menus', { id: this.$el });
+                    this.$dispatch('close-other-menus', { id: this.$el });
 
-                this.$nextTick(() => {
-                    this.$refs.contextmenu.style.opacity = 1;
-                    this.calculateContextMenuPosition(event);
-                });
-            },
+                    this.$nextTick(() => {
+                        this.$refs.contextmenu.style.opacity = 1;
+                        this.calculateContextMenuPosition(event);
+                    });
+                },
 
-            calculateContextMenuPosition: function(clickEvent) {
-                const menu = this.$refs.contextmenu;
-                const menuHeight = menu.offsetHeight;
-                const menuWidth = menu.offsetWidth;
+                calculateContextMenuPosition: function(clickEvent) {
+                    const menu = this.$refs.contextmenu;
+                    const menuHeight = menu.offsetHeight;
+                    const menuWidth = menu.offsetWidth;
 
-                const top = clickEvent.clientY + menuHeight > window.innerHeight ?
-                    window.innerHeight - menuHeight :
-                    clickEvent.clientY;
+                    const top = clickEvent.clientY + menuHeight > window.innerHeight ?
+                        window.innerHeight - menuHeight :
+                        clickEvent.clientY;
 
-                const left = clickEvent.clientX + menuWidth > window.innerWidth ?
-                    clickEvent.clientX - menuWidth :
-                    clickEvent.clientX;
+                    const left = clickEvent.clientX + menuWidth > window.innerWidth ?
+                        clickEvent.clientX - menuWidth :
+                        clickEvent.clientX;
 
-                menu.style.top = `${top}px`;
-                menu.style.left = `${left}px`;
-            },
+                    menu.style.top = `${top}px`;
+                    menu.style.left = `${left}px`;
+                },
 
-            handleCloseOtherMenus: function(event) {
-                if (event.detail.id !== this.$el) {
-                    this.contextMenuOpen = false;
+                handleCloseOtherMenus: function(event) {
+                    if (event.detail.id !== this.$el) {
+                        this.contextMenuOpen = false;
+                    }
+                },
+
+                init: function() {
+                    window.addEventListener('resize', () => { this.contextMenuOpen = false; });
                 }
-            },
-
-            init: function() {
-                window.addEventListener('resize', () => { this.contextMenuOpen = false; });
             }
         }
-    }
-</script>
+    </script>
+
+@else
+    <div>
+        @include($getMainView())
+    </div>
+@endif
