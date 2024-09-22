0e36a30bb0fd5e6c9ee4e7208c958eeff1cd942c
aymanalhattami <aymanalhattami@users.noreply.github.com>
2024-09-22 08:51:03 +0000

    Fix styling

--- src/Columns/ContextMenuToggleColumn.php
+++ src/Columns/ContextMenuToggleColumn.php
@@ -3,7 +3,6 @@
 namespace AymanAlhattami\FilamentContextMenu\Columns;
 
 use AymanAlhattami\FilamentContextMenu\Traits\ColumnHasContextMenu;
-use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ToggleColumn;
 
 class ContextMenuToggleColumn extends ToggleColumn
